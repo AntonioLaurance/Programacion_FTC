733e070da227e4613e224a049cbf4a70837f1ded
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2020-11-22 12:28:22 -0600

    Update Teleoperado.java

--- Teleoperado.java
+++ Teleoperado.java
@@ -1,12 +1,42 @@
 # Programacion_FTC
 
-private motor1 = null;
-private motor2 = null;
+package org.firstinspires.ftc.teamcode;
 
-// comienzo
-waitForStart();
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-  
-public static void main(String[] args){
 
+@TeleOp
+
+public class TeleOperado extends LinearOpMode{
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+    
+    // Método waitForStart();
+    waitForStart();
+    runtime.reset();
+    double position = 0;
+    
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+    
+    while(opModelsActive()){
+        // elegir potencia 
+    }
+
+    // todo: write your code here
+    
+}
+
+
+
+}
 }
