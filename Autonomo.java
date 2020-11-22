29374fc3db365e3737562d251dbe75695eadd105
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2020-11-22 12:26:04 -0600

    Update Autonomo.java

--- Autonomo.java
+++ Autonomo.java
@@ -1,7 +1,34 @@
 # Programacion_FTC_Sputnik
 
-private motor1 = null;
+package org.firstinspires.ftc.teamcode;
 
-public static void main(String[] args){
-  
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous
+
+public class Autonomo extends LinearOpMode{
+
+    // todo: write your code here
+    
+    // definir las variables de nuestros motores
+    private DcMotor motor1 = null;
+    private DcMotor motor2 = null;
+    
+    
+    public void runOpMode(){
+        
+    }
+    
+    private void moverDistanciaRecta(double distancia){
+        
+    }
+    
+    private void moverLateralmente(double distancia){
+        
+    }
+    
+    
 }
