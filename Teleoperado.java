d225837d36f7606f1953123355912861de294a28
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2021-02-06 19:14:20 -0600

    Update Teleoperado.java

--- Teleoperado.java
+++ Teleoperado.java
@@ -1,4 +1,4 @@
-# Programacion_FTC
+// Programacion_FTC
 // Métodos de Java de FTC: http://ftctechnh.github.io/ftc_app/doc/javadoc/index.html
 
 package org.firstinspires.ftc.teamcode;
@@ -10,7 +10,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
     
 
 @TeleOp
-
 public class TeleOperado extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeft = null;
@@ -18,22 +17,95 @@ public class TeleOperado extends LinearOpMode{
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
     
-    // Método waitForStart();
-    waitForStart();
-    runtime.reset();
-    double position = 0;
     
     @Override
     public void runOpMode(){
         telemetry.addData("Status", "Initialized");
         telemetry.update();
     }
-    
-    while(opModelsActive()){
-        // elegir potencia 
+
+    // Dar distancias en cm (rad/m)
+    private void moverDistanciaRecta(double distancia){
+      frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+      // Calculo del diametro de las ruedas
+      // Ver dirección de los motores
+      frontLeft.setTargetPosition((int)(distancia*288/9));
+      frontRight.setTargetPosition((int)(distancia*288/9d));
+      backLeft.setTargetPosition((int)(distancia*288/9));
+      backRight.setTargetPosition((int)(distancia*288/9d));
+
+      
+      while (opModeIsActive() && frontLeft.isBusy() && frontRight.isBusy()){
+
+        frontLeft.setPower(0.75);
+        frontRight.setPower(0.75);
+        backLeft.setPower(0.75);
+        backRight.setPower(0.75);
+
+      }
+
     }
+    
+    private void moverLateralmente(double distancia){
+        
+    }
+    
+    frontLeft = hardwareMap.get(DcMotor.class, "left_front_drive");
+    frontRight = hardwareMap.get(DcMotor.class, "right_front_drive");
+    backLeft = hardwareMap.get(DcMotor.class, "back_left_drive");
+    backRight = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+
+    // Reseteo del encoder
+    frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    frontRight.setMode(DcMotor.RunMode.RUM_WITHOUT_ENCODER);
+    backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    
+            // elegir potencia
+
+        //setPower();
+
+    //setDirection();
+    frontLeft.setDirection(DcMotor.Direction.FORWARD);
+    frontRight.setDirection(DcMotor.Direction.REVERSE);
+    backLeft.setDirection(DcMotor.Direction.FORWARD);
+    backRight.setDirection(DcMotor.Direction.REVERSE); 
+
+    // Método waitForStart();
+    waitForStart();
+    runtime.reset();
+    double position = 0;
+
 
     // todo: write your code here
+    while(opModeIsActive()){
+      double frontLeftPower;
+      double frontRightPower;
+      double backLeftPower;
+      double backRightPower;
+      double drive = - gamepad1.left_stick_y;
+      double lateral = gamepad1.left_stick_x;
+      double turn = gamepad1.right_stick_x;
+
+      // Asignación de las potencias
+      frontLeftPower = drive + lateral + turn;
+      frontRightPower = drive - lateral - turn;
+      backLeftPower = drive - lateral + turn;
+      backRightPower = drive + lateral - turn;
+
+      frontLeft.setPower(frontLeftPower);
+      frontRight.setPower(frontRightPower);
+      backLeft.setPower(backLeftPower);
+      backRight.setPower(backRightPower);
+
+    }
+
+    
     
 }
 
