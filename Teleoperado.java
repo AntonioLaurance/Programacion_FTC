d57f401e97f5f6b25c1e803ae36ee36868d75d53
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2021-03-13 16:51:55 -0600

    Update Teleoperado.java

--- Teleoperado.java
+++ Teleoperado.java
@@ -4,6 +4,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -20,61 +21,27 @@ public class TeleOperado extends LinearOpMode{
     
     @Override
     public void runOpMode(){
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+      telemetry.addData("Status", "Initialized");
+      telemetry.update();
+
+      // Reconocimiento de motores
+      frontLeft = hardwareMap.get(DcMotor.class, "left_front_drive");
+      frontRight = hardwareMap.get(DcMotor.class, "right_front_drive");
+      backLeft = hardwareMap.get(DcMotor.class, "back_left_drive");
+      backRight = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+      // Reseteo del encoder
+      frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-
-    // Dar distancias en cm (rad/m)
-    private void moverDistanciaRecta(double distancia){
-      frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-      // Calculo del diametro de las ruedas
-      // Ver dirección de los motores
-      frontLeft.setTargetPosition((int)(distancia*288/9));
-      frontRight.setTargetPosition((int)(distancia*288/9d));
-      backLeft.setTargetPosition((int)(distancia*288/9));
-      backRight.setTargetPosition((int)(distancia*288/9d));
-
-      
-      while (opModeIsActive() && frontLeft.isBusy() && frontRight.isBusy()){
-
-        frontLeft.setPower(0.75);
-        frontRight.setPower(0.75);
-        backLeft.setPower(0.75);
-        backRight.setPower(0.75);
-
-      }
-
-    }
-    
-    private void moverLateralmente(double distancia){
-        
-    }
-    
-    frontLeft = hardwareMap.get(DcMotor.class, "left_front_drive");
-    frontRight = hardwareMap.get(DcMotor.class, "right_front_drive");
-    backLeft = hardwareMap.get(DcMotor.class, "back_left_drive");
-    backRight = hardwareMap.get(DcMotor.class, "back_right_drive");
-
-
-    // Reseteo del encoder
-    frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    frontRight.setMode(DcMotor.RunMode.RUM_WITHOUT_ENCODER);
-    backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    
-            // elegir potencia
-
-        //setPower();
 
     //setDirection();
     frontLeft.setDirection(DcMotor.Direction.FORWARD);
     frontRight.setDirection(DcMotor.Direction.REVERSE);
-    backLeft.setDirection(DcMotor.Direction.FORWARD);
-    backRight.setDirection(DcMotor.Direction.REVERSE); 
+    backLeft.setDirection(DcMotor.Direction.REVERSE);
+    backRight.setDirection(DcMotor.Direction.FORWARD); 
 
     // Método waitForStart();
     waitForStart();
@@ -98,18 +65,11 @@ public class TeleOperado extends LinearOpMode{
       backLeftPower = drive - lateral + turn;
       backRightPower = drive + lateral - turn;
 
+      // elegir potencia
+      //setPower();
       frontLeft.setPower(frontLeftPower);
       frontRight.setPower(frontRightPower);
       backLeft.setPower(backLeftPower);
       backRight.setPower(backRightPower);
 
-    }
-
-    
-    
-}
-
-
-
-}
-}
+    }   
